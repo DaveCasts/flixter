821d405944eade3575d4dcd601bd65d7c67e9f35
Dave Campos <dacamposc96@gmail.com>
2019-10-04 02:04:36 +0000

    Added Css and validates the title description and cost

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: {minimum: 3, maximum: 100}
+  validates :description, presence: true, length: {minimum: 3, maximum: 250}
+  validates :cost, presence:true, numericality: {greater_than_or_equal_to: 0}
 end
