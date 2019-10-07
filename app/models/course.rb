1b46b4bcf2cf820f4e6d54f3f129a09e082ce883
Dave Campos <dacamposc96@gmail.com>
2019-10-07 23:55:36 +0000

    Added a new section page and sections are viewable to student and teachers through a view as student button

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :user
+  has_many :sections
 
   validates :title, presence: true, length: {minimum: 3, maximum: 100}
   validates :description, presence: true, length: {minimum: 3, maximum: 250}
