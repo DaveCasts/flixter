c16aec495fd737391353b228143018cae1a1d22f
Dave Campos <dacamposc96@gmail.com>
2019-10-24 01:23:52 +0000

    create lesson controllers

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,13 @@
+class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  def show 
+    current_lesson
+  end
+
+  private
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+end
